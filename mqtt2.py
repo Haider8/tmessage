fc8b9a673bcf8289032e2c1e68f53f6086034381
Haider Ali <haider.lee23@gmail.com>
2019-09-06 01:33:12 +0530

    Successfully sending and receiving messages

--- mqtt2.py
+++ mqtt2.py
@@ -4,21 +4,34 @@ MQTT_TOPIC = "amu"
 BROKER_ENDPOINT = "192.168.43.235"
 BROKER_PORT = 1883
 
+
 mqtt_client = mqtt.Client()
+current_msg = ""
+current_user = "Haider8"
 
 def on_message(client, userdata, message):
-   print("Message Recieved from broker: " + message.payload.decode("utf-8"))
+    current_msg = message.payload.decode("utf-8")
+    user = current_msg.partition('[')[-1].rpartition(']')[0]  # to get the username between []
+    if current_msg != message.payload.decode("utf-8"):
+        print(user + current_msg)
+    
+    if user != current_user:
+        print(current_msg)
 
 def main():
     try:
         mqtt_client.on_message = on_message
         mqtt_client.connect(BROKER_ENDPOINT, BROKER_PORT)
         mqtt_client.subscribe(MQTT_TOPIC)
-        mqtt_client.loop_forever()
+        mqtt_client.loop_start()
+        while True:
+            temperature = str(input())
+            current_msg = temperature
+            mqtt_client.publish(MQTT_TOPIC, temperature)
     except KeyboardInterrupt:
         mqtt_client.disconnect()
         print('\ngoodbye !')
-    
+
 
 if __name__ == '__main__':
     main()
