bd441149aa17c21141b82fbbc222547484b092df
ShanmugaPriya <priya97a@gmail.com>
2019-10-08 16:46:23 +0530

    [WIP] Fix pylint errors (#40)
    * increased pylint rating from 4.92/10 to 6.64/10
    
    * increased pylint rating from 6.64/10 to 7.52/10
    
    * added pylint ga
    
    * removed pylint ga
    
    * added travis ci
    
    * travis ci fixes
    
    * travis ci fixes

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
+""" Setup for tmessage CLI """
 from setuptools import setup
 
 
 def get_requirements():
-    with open('requirements.txt', 'r') as f:
-        raw_requirements = f.readlines()
+    """ Fetch the requirements from requirements.txt """
+    with open('requirements.txt', 'r') as file:
+        raw_requirements = file.readlines()
     requirements = [requirement.strip() for requirement in raw_requirements]
     return requirements
 
 
 setup(name='tmessage',
       entry_points={
-        'console_scripts': ['tmessage=tmessage.cli:main']
+          'console_scripts': ['tmessage=tmessage.cli:main']
       },
       description="""
       This is a lightweight and low bandwidth
--- tmessage/auth.py
+++ tmessage/auth.py
@@ -1,3 +1,4 @@
+""" Auth : Register new user, check or authenticate existing user """
 import os
 import jwt
 import requests as r
@@ -8,19 +9,20 @@ API_BASE_URL = os.environ.get("TMESSAGE_API_URL") or "https://peaceful-waters-15
 API_USER_URL = f'{API_BASE_URL}/api/user'
 
 
-def checkExisted(user_name):
-    ENDPOINT_URL = f'{API_USER_URL}/checkExist/{user_name}'
-    response = r.get(ENDPOINT_URL)
+def check_existed(user_name):
+    """ Check if  user already exist """
+    endpoint_url = f'{API_USER_URL}/checkExist/{user_name}'
+    response = r.get(endpoint_url)
     if response.status_code == 200:
         data = response.json()
         return data['exist']
-    else:
-        error = response.json()
-        raise Exception(f'Error Verifying User: {error["message"]}')
+    error = response.json()
+    raise Exception(f'Error Verifying User: {error["message"]}')
 
 
 def authenticate(user_name, password):
-    ENDPOINT_URL = f'{API_USER_URL}/login'
+    """ Authenticate already registered User """
+    endpoint_url = f'{API_USER_URL}/login'
     headers = {
         "Content-Type": "application/json"
     }
@@ -28,30 +30,29 @@ def authenticate(user_name, password):
         "user_name": user_name,
         "password": password
     }
-    response = r.post(ENDPOINT_URL, json=credential, headers=headers)
+    response = r.post(endpoint_url, json=credential, headers=headers)
     if response.status_code == 200:
         data = response.json()
         return jwt.decode(data['token'], verify=False)
-    else:
-        error = response.json()
-        raise Exception(f'Error Authenticating User: {error["message"]}')
+    error = response.json()
+    raise Exception(f'Error Authenticating User: {error["message"]}')
 
 
 def register(user_name, displayed_name, password, password_confirm):
-    ENDPOINT_URL = f'{API_USER_URL}/register'
+    """ Register a new User """
+    endpoint_url = f'{API_USER_URL}/register'
     headers = {
         "Content-Type": "application/json"
     }
-    newUser = {
+    new_user = {
         "user_name": user_name,
         "displayed_name": displayed_name,
         "password": password,
         "password_confirm": password_confirm
     }
-    response = r.post(ENDPOINT_URL, json=newUser, headers=headers)
+    response = r.post(endpoint_url, json=new_user, headers=headers)
     if response.status_code == 200:
         data = response.json()
         return jwt.decode(data['token'], verify=False)
-    else:
-        error = response.json()
-        raise Exception(f'Error Registering User: {error["message"]}')
+    error = response.json()
+    raise Exception(f'Error Registering User: {error["message"]}')
