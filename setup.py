380960358a152ece275e8d194b2fd1f82dd9e780
jcsekar <56619570+jcsekar@users.noreply.github.com>
2019-10-23 15:15:45 +0530

    configured pre-commit (#52)
    * configured pre-commit
    
    * removed pre-commit version number
    
    * removed version for pre-commit in setup.py
    
    * addressed review comments

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!python
-
 from setuptools import setup
 
 
@@ -15,32 +14,34 @@ def get_requirements():
         'requests==2.22.0',
         'PyJWT==1.7.1',
         'python-dotenv==0.10.3',
-        'simple-chalk==0.1.0'
+        'simple-chalk==0.1.0',
     ]
     return requirements
 
 
-setup(name='tmessage',
-      version="0.0.2",
-      author="Haider Ali",
-      author_email="haider.lee23@gmail.com",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/Haider8/tmessage",
-      license="GPL-3.0",
-      entry_points={
-          'console_scripts': ['tmessage=tmessage.cli:main']
-      },
-      description="""
+setup(
+    name='tmessage',
+    version="0.0.2",
+    author="Haider Ali",
+    author_email="haider.lee23@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Haider8/tmessage",
+    license="GPL-3.0",
+    entry_points={
+        'console_scripts': ['tmessage=tmessage.cli:main'],
+    },
+    description="""
       This is a lightweight and low bandwidth
       CLI tool which can be used for group
       communication right from your terminal.
       """,
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "Operating System :: OS Independent",
-      ],
-      python_requires='>=3.4',
-      packages=['tmessage'],
-      install_requires=get_requirements(),
-      zip_safe=False)
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+    packages=['tmessage'],
+    install_requires=get_requirements(),
+    zip_safe=False,
+)
