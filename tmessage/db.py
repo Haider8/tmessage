ecf0b847369e5cdd3c47368ad8aa1a6a9300415e
HashirGJ8842 <56411583+HashirGJ8842@users.noreply.github.com>
2019-10-27 15:58:23 +0530

    Update db.py

--- tmessage/db.py
+++ tmessage/db.py
@@ -1,28 +1,32 @@
 """Handles the connection to the SQLite database as well as DB interaction"""
 from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-from peewee import CharField, DateTimeField, Model, SqliteDatabase
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config['SQLALCHEMY_BINDS'] = { 'we' : 'sqlite:///we.db'}
+db = SQLAlchemy(app)
+
+User = ''
+class MessageDatabase(db.Model):
+    __bind_key__ = User 
+    sender = db.Column(db.String(120), primary_key=True, nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    timestamp = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f"MessageDatabase('{self.sender}','{self.message}','{self.timestamp}')"
 
 
-MESSAGES_DB = SqliteDatabase('message_store.sqlite')
-
-
-class Message(Model):
-    """Message table - keeps track of message sent and received"""
-    sender = CharField()
-    message = CharField()
-    timestamp = DateTimeField()
-
-    class Meta:  # pylint: disable=missing-class-docstring,too-few-public-methods
-        database = MESSAGES_DB
-
-
-@MESSAGES_DB
-def store_messages(user, raw_msg):
+def store_messages(user, raw_msg, new):
+    User = user
     """Store a message sent by the indicated user in the database"""
+
+    if new == 'yes':
+        app.config['SQLALCHEMY_BINDS'][User] = f'sqlite:///{User}.db'
     time = datetime.now()
-
-    Message.create(sender=user, message=raw_msg, timestamp=time)
-
-
-MESSAGES_DB.create_tables([Message])
+    db.create_all()
+    x = MessageDatabase(sender=User, message=raw_msg, timestamp=time)
+    db.session.add(x)
+    db.session.commit()
