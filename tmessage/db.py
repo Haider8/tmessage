0c9c00c3dd04befcdfac4391a61671f71fb27fab
HashirGJ8842 <56411583+HashirGJ8842@users.noreply.github.com>
2019-10-28 00:08:27 +0530

    Update db.py

--- tmessage/db.py
+++ tmessage/db.py
@@ -11,19 +11,24 @@ DB = SQLAlchemy(APP)
 
 
 class Message(DB.Model):
+    """ Database where messages will be stored """
     sender = DB.Column(DB.String(120), primary_key=True, nullable=False)
     message = DB.Column(DB.String(120), nullable=False)
     timestamp = DB.Column(DB.DateTime)
 
+    def add(self):
+        return 1+1
     def __repr__(self):
         return f"Message('{self.sender}','{self.message}','{self.timestamp}')"
 
 
 def new_database(user):
+    """ creates new database for new users """
     APP.config['SQLALCHEMY_BINDS'][user] = f'sqlite:///{user}.db'
-    
-    
+
+
 def store_messages(user, raw_msg):
+    """ function that stores messages from the particular user """
     Message.__bind_key__ = user
     time = datetime.now()
     DB.create_all()
